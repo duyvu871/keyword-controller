dd58bf7c66593c4aa68cccc5968362037c5be828
duyvu871 <zoocp123@gmail.com>
2023-12-05 13:29:55 +0700

    add direct logic for banner [feature]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,11 @@ import {usePostData} from "@/hooks/usePostData";
 import {IPostData as PostData } from "@/types/postType";
 import {tw} from "@/ultis/tailwind-ultis";
 import {BannerWrapper} from "@/components/Banner";
+import Link from "next/link";
+
+import {Redirect_banner_ads} from "@/resources/redirect_banner_ads"
+import {randomList} from "@/ultis/random";
+import React from "react";
 
 const PostWrapper = ({
 postList, isGridLayout, isCompact
@@ -41,6 +46,19 @@ postList, isGridLayout, isCompact
 }
 
 const BannerGroup = () => {
+    const [isShowBanner, setIsShowBanner] = React.useState<boolean>(true);
+
+    const redirectUrl = randomList(Redirect_banner_ads);
+
+    const directToAdsPage = () => {
+        setIsShowBanner(false);
+        localStorage.setItem("isShowBanner", "false");
+        const anchor = document.createElement('a');
+        anchor.href = redirectUrl;
+        anchor.target="_blank";
+        anchor.click();
+
+    }
 
     return (
         <div className={tw(
@@ -55,33 +73,47 @@ const BannerGroup = () => {
                     right: "0",
                     bottom: "0",
                 }}
+                isShow={isShowBanner}
+                content={"banner 1"}
+
             >
-                <div className={tw(
-                    "flex flex-col justify-center items-center gap-4",
-                    "bg-white p-4 rounded-md"
-                )}>
-                    <Image
-                        src={"/bongda1.png"}
-                        width={600}
-                        height={200}
-                        alt={"logo"}
-                    />
-                    <div className={tw(
-                        "flex flex-col justify-center items-center gap-4",
-                        "text-center"
-                    )}>
-                        <h1 className={tw(
-                            "text-2xl text-black font-bold text-center"
-                        )}>
-                            Chào mừng bạn đến với trang web của chúng tôi
-                        </h1>
-                        <h2 className={tw(
-                            "text-lg text-black font-bold text-center"
-                        )}>
-                            Chúng tôi sẽ cập nhật những bài đăng mới nhất cho bạn
-                        </h2>
-                    </div>
-                </div>
+
+               {/*<Link href={randomList(Redirect_banner_ads)} target={"_blank"}>*/}
+
+                   <div className={tw(
+                       "flex flex-col",
+                       "bg-white p-4 rounded-md pt-[5px] px-[5px]"
+                   )}>
+                       <div className={tw(
+                           "flex flex-col items-end mb-1",
+                       )}>
+                           <p onClick={() => {directToAdsPage()}} className={"cursor-pointer text-end gap-4 text-md text-white font-bold rounded-full bg-black leading-[0.7] p-1 w-fit"}>&times;</p>
+                       </div>
+                       <Link href={redirectUrl} target={"_blank"}>
+                           <Image
+                               src={"/bongda1.png"}
+                               width={600}
+                               height={200}
+                               alt={"logo"}
+                           />
+                           <div className={tw(
+                               "flex flex-col justify-center items-center gap-4",
+                               "text-center"
+                           )}>
+                               <h1 className={tw(
+                                   "text-2xl text-black font-bold text-center"
+                               )}>
+                                   Chào mừng bạn đến với trang web của chúng tôi
+                               </h1>
+                               <h2 className={tw(
+                                   "text-lg text-black font-bold text-center"
+                               )}>
+                                   Chúng tôi sẽ cập nhật những bài đăng mới nhất cho bạn
+                               </h2>
+                           </div>
+                       </Link>
+                   </div>
+               {/*</Link>*/}
             </BannerWrapper>
         </div>
     )
